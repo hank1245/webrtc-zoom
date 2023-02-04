1b5e4f5dc43bf69cc0e018328cff3fb564cad5fe
Hank Kim <hank29206880@gmail.com>
2023-02-04 17:43:51 +0900

    websocket connection established

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -9,4 +11,18 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-app.listen(5000, () => console.log("server running port 5000"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", (message) => {
+    console.log(Buffer.from(message, "base64").toString("utf-8"));
+    socket.send("recevied your message");
+  });
+  socket.send("Hello!");
+});
+
+server.listen(5000, () => console.log("server running"));
